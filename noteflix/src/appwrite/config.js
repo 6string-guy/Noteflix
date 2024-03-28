5b270a30ca948be80ff6364f04c11b2a2e34dec6
Sanskar <sanskarjaiswal010@gmail.com>
2024-03-29 02:36:30 +0530

    appwrite setup for downloading and uploading

---
+++ noteflix/src/appwrite/config.js
@@ -0,0 +1,86 @@
+import {Client, ID, Databases, Storage, Query} from "appwrite"
+
+export class Service{
+    client =new Client()
+    database;
+    bucket;
+    constructor()
+    {
+        this.client
+            .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
+            .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
+            this.database=  new Databases( client)
+            this.bucket =new Storage( client)
+    }
+
+    async createPost ({title,slug, content, featuredImage,status, userId})
+    {
+        try {
+
+            await this.database.createDocument(
+                import.meta.env.VITE_APPWRITE_DATABASE_ID,
+                import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+                slug,
+                {title,content, featuredImage, status, userId}
+            )
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+    async updatePost(slug,{title, content, featuredImage,status})
+    {
+        try {
+
+            return await this.database.updateDocument(
+                import.meta.env.VITE_APPWRITE_DATABASE_ID,
+                import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+                slug,
+                {
+                    title, content, featuredImage, status
+                }
+            )
+            
+        } catch (error) {
+            throw error
+        }
+
+    }
+    async deletePost(slug)
+    {
+        try {
+            await this.database.deleteDocument(
+                import.meta.env.VITE_APPWRITE_DATABASE_ID,
+                import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+                slug
+            )
+
+            return true
+            
+        } catch (error) {
+           console.log( "Deletion failed")
+            return false;
+        }
+    }
+    async getPost ( slug)
+    {
+        try{
+            return await this.database.getDocument(
+                import.meta.env.VITE_APPWRITE_DATABASE_ID,
+                import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+                slug
+
+            )
+        }
+        catch(error)
+        {
+            throw error
+        }
+    }
+    async getPosts( ){
+        
+    }
+}
+const service =new Service()
+
+export default service;
